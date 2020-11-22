b5d22fb4226752e12006947d16d4c6f6283712cf
OlegKirduk <olegkirduk@mail.ru>
2020-11-22 18:45:32 +0300

    объявление переменной и присвоение ей значения

--- script/script.js
+++ script/script.js
@@ -7,3 +7,9 @@ console.log(`Я учу JS`);
 console.log('js');
 
 /* и никто мне ничего не сделает */
+
+/* объявление переменной и присвоение ей значения*/
+let myNick = 'Oleg';
+let myAge = 32;
+let myLogo = 'Be Happy!';
+console.log(myNick + myAge);
